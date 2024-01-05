f7f598020c0fea4be79341f965a65c94c0328e77
Srinivasan <155220660+HeloSrini@users.noreply.github.com>
2024-01-05 21:47:30 +0530

    Desktop

---
+++ main.js
@@ -0,0 +1,7 @@
+var type = new Typed(".text", {
+    strings: ["Front-End Developer", "Web Developer", "Software Developer", "Cloud Engineer"],
+    typeSpeed: 80,
+    backSpeed: 60,
+    backDelay: 70,
+    loop: true
+});
